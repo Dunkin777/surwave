4752b418f944093c5cd684ef3220388bc416e168
Zhanna Fedorova <zhanna_fedorova@epam.com>
2019-12-28 22:22:19 +0300

    21-added DataSourse configuration and initial sql script for flyway

---
+++ src/main/java/epamers/surwave/configuration/PostgresConfiguration.java
@@ -0,0 +1,35 @@
+package epamers.surwave.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PostgresConfiguration {
+    @Value("${POSTGRES_DB_HOST:localhost}")
+    private String host;
+
+    @Value("${POSTGRES_DB:surwave}")
+    private String databaseName;
+
+    @Value("${POSTGRES_USER:postgres}")
+    private String user;
+
+    @Value("${POSTGRES_PASSWORD:postgres}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(String.format("jdbc:postgresql://%s:5432/%s", host, databaseName));
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.username(user);
+        dataSourceBuilder.password(password);
+
+        return dataSourceBuilder.build();
+    }
+}
