d1c9c1532e60c36b4a2417fe70e98ca7e3af4866
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2020-02-17 11:49:34 +0300

    Changeset for unused tables removal Renamed SurveyUserSong to Option

--- src/main/resources/db/migration/V004__db_schema_update.sql
+++ src/main/resources/db/migration/V004__db_schema_update.sql
@@ -1,4 +1,5 @@
 drop table if exists survey_song;
 drop table if exists user_song;
 
-alter table survey_user_song rename to option;
+alter table if exists song drop column survey_id;
+alter table if exists survey_user_song rename to option;
