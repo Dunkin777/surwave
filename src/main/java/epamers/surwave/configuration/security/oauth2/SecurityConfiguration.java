d7077e8191dc8c11b8b1f2b3c27c3dfce58394fe
Oboist <englishhorn@mail.ru>
2020-03-08 22:23:24 +0300

    SRWV-HOTFIX - remove unnecessary permissions from auth2

--- src/main/java/epamers/surwave/configuration/security/oauth2/SecurityConfiguration.java
+++ src/main/java/epamers/surwave/configuration/security/oauth2/SecurityConfiguration.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableOAuth2Sso
@@ -20,7 +18,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("swagger-ui.html", "/swagger**", "/webjars/**", "/v2/api-docs**").permitAll()
         .anyRequest().authenticated()
         .and()
         .csrf().disable();
