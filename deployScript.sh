dcd999c23676f4768b6f89ca2873f6ddc9f6180e
Алексей Положенцев (Oboist) <englishhorn@mail.ru>
2020-03-25 10:14:13 +0000

    SRWV-111 - rename container and image, add network

--- deployScript.sh
+++ deployScript.sh
@@ -3,6 +3,6 @@ cd /home/surwave/surwave-be
 git checkout Develop
 git pull
 ./gradlew clean build -x test
-docker-compose build core
-docker stop surwave
-docker-compose up -d core
+docker-compose build
+docker stop surwave-be
+docker-compose up -d
