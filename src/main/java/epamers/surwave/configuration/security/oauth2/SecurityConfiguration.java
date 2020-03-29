3af45df88013856681fc741df50044b769717a88
Oboist <englishhorn@mail.ru>
2020-03-29 17:18:30 +0300

    SRWV-130 - add exception and profile

--- src/main/java/epamers/surwave/configuration/security/oauth2/SecurityConfiguration.java
+++ src/main/java/epamers/surwave/configuration/security/oauth2/SecurityConfiguration.java
@@ -18,6 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .antMatchers("/user/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .csrf().disable();
